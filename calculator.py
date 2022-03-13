80d206f3ebdb4902b97cebe4c8a57781c0328e98
acmota2 <acmota2@gmail.com>
2022-03-13 23:30:16 +0000

    Added help command

--- calculator.py
+++ calculator.py
@@ -24,10 +24,31 @@ def __main__():
         expression: str = input('input > ')
         if not expression:
             print('''Expressão nula e/ou vazia
-Deverá receber um input do tipo [NUMERO][OPERADOR][NUMERO]''')
+
+Para mais informações escrever "help"|"h"''')
             continue
-        elif expression == 'quit':
+        elif expression in ('quit', 'q'):
             break
+        elif expression in ('help', 'h'):
+            print('''
+********************
+|                  |
+|   py-calculator  |  by: acmota2
+|                  |
+********************
+
+Esta calculadora permite calcular coisas segundo a regra PEMDAS com os seguintes operadores:
+    +   : soma          (x + y)
+    -   : subtração     (x - y)
+    *   : multiplicação (x * y)
+    /   : divisão       (x / y)
+    ^   : potência      (x ^ y)
+    v   : raiz          (x v y | v x)
+    !   : fatorial      (x !)
+
+É também possível efetuar operações do tipo "2(6 + 2)"
+''')
+            continue
         ans = yacc.parse(expression)
         print(ans)
 
